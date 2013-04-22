65eca1e8e8d659be74c29c9a8a50d3df8d3a9859
= <alexmccracken55@gmail.com>
2013-04-22 17:19:49 -0400

    delete and update vehicle type

---
+++ src/main/java/edu/thangiah/action/vehicletype/DeleteAction.java
@@ -0,0 +1,53 @@
+package edu.thangiah.action.vehicletype;
+
+
+import edu.thangiah.entity.VehicleType;
+
+public class DeleteAction extends ManagementController
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5049171011473675910L;
+
+
+	@Override
+	public void prepare() throws Exception {
+		this.mode = Modes.DELETE;
+		super.prepare();
+	}
+	
+	
+	@Override
+    public String execute(){
+		// Errors could be thrown by the ManagementController prepare methods.
+    	if( this.hasControllerErrors() ){
+    		return ERROR;
+    	}
+    	
+    	if( id <= 0 ){
+    		this.addActionError("Must specify a target for deletion");
+    		return INPUT;
+    	}
+    	
+    	VehicleType fromDb = this.retrieveEntityById(vehicleTypeDao, id);
+    	if( fromDb == null ){
+    		this.addActionError("This vehicle type does not exist.");
+    		return INPUT;
+    	}
+    	
+    	try{
+    		vehicleTypeDao.delete(fromDb);
+    		if(fromDb !=null)
+    		{
+    			this.addActionError("A dependency exists that is prohibiting this deleteion");
+    		}
+    	}
+    	catch( Exception e ){
+    		this.addActionError("A dependency exists that is prohibiting this deleteion");
+    		return INPUT;
+    	}
+    	
+    	return SUCCESS;
+    }
+}
---
+++ src/main/java/edu/thangiah/action/vehicletype/UpdateAction.java
@@ -0,0 +1,54 @@
+package edu.thangiah.action.vehicletype;
+
+import java.util.List;
+
+import edu.thangiah.entity.Vehicle;
+import edu.thangiah.entity.VehicleType;
+
+public class UpdateAction extends ManagementController{
+
+	private static final long serialVersionUID = 4634172821230672694L;
+
+	@Override
+	public void prepare() throws Exception {
+		this.mode =  Modes.EDIT;
+		super.prepare();
+	}
+	
+	
+	@Override
+    public String execute(){
+		// Errors could be thrown by the ManagementController prepare methods.
+    	if( this.hasControllerErrors() ){
+    		return ERROR;
+    	}
+    	
+    	VehicleType fromDb = this.retrieveEntityById(vehicleTypeDao, id);
+    	if( fromDb == null ){
+    		this.addActionError("A vehicle type must be specified");
+    		return INPUT;
+    	}
+    	
+    	VehicleType fromForm = this.getEntity();
+    	if( fromForm == null ){
+    		this.addActionError("The form data could not be retrieved from the form.  Please try again.");
+    		return INPUT;
+    	}
+    	
+		List<Vehicle> fromDbList = vehicleDao.findByVehicleType(this.getVehicleType());
+		if( fromDbList.size() != 0){
+					this.addFieldError("vehicles", "There are vehicles connected to this type preventing deletion");
+					return INPUT;
+			}
+    	fromDb.merge(fromForm);
+    	vehicleTypeDao.update(fromDb);
+    	return SUCCESS;
+	    }
+
+
+	@Override
+	public void validate(){
+		//validate up in here
+    }
+	
+}
